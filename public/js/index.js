08db76bb0b7d272661d368cc342bc8e710f799d2
Scott Allan <sallan306@gmail.com>
2019-04-18 19:16:49 -0700

    reformatted many things, changed the navbar

--- public/js/index.js
+++ public/js/index.js
@@ -1,20 +1,27 @@
 
 $("#loginButton").click(function(){ window.location.href="/login";});
-$("#createAccountButton").click(function(){ window.location.href="/createaccount";});
-$("#breweriesButton").click(function(){ window.location.href="/breweries";});
+$("#navLoginButton").click(function(){ window.location.href="/login";});
 
 $("#backButton").click(function(){ window.location.href="/";});
+$("#navHomePageButton").click(function(){ window.location.href="/";});
 
+$("#createAccountButton").click(function(){ window.location.href="/createaccount";});
+$("#navCreateAccountButton").click(function(){ window.location.href="/createaccount";});
 
+$("#breweriesButtonHome").click(function(){ window.location.href="/breweries";});
+$("#breweriesButtonDrinks").click(function(){ window.location.href="/breweries";});
 
-// $(".homeButton").click(function(){ window.location.href="/";});
-// $("#homeButtonLogin").click(function(){ window.location.href="/";});
-// $("#homeButtonCreate").click(function(){ window.location.href="/";});	
+$("#navBreweriesButton").click(function(){ window.location.href="/admin/me";});
+$("#navLogoutButton").click(function(){ window.location.href="/logout";});
 
 function openNav() {
 	document.getElementById("mySidenav").style.display = "block";
+	document.getElementById("mySidenav").style.visibility = "visible";
+	document.getElementById("mySidenav").style.opacity = "1";
 }
   
 function closeNav() {
-	document.getElementById("mySidenav").style.display = "none";
+	// document.getElementById("mySidenav").style.display = "none";
+	document.getElementById("mySidenav").style.visibility = "hidden";
+	document.getElementById("mySidenav").style.opacity = "0";
 }
